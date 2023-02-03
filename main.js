df95226585a36af7d9935f160c1755bea863ab92
misha <dedovmu@gmail.com>
2023-02-03 20:37:35 +0300

    Gap in points example

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ import {
 	PerspectiveCamera,
 	WebGLRenderer,
 	MeshBasicMaterial,
-	BufferAttribute,
 	BufferGeometry,
 	Vector3,
-	Cylindrical,
-	Points
+	Points,
 } from 'three'
-import { useMode, modeLch, modeOklch } from 'culori'
+import { useMode, modeLch } from 'culori'
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
 
 let scene, camera, renderer, controls
@@ -21,99 +19,34 @@ function init() {
 
 	renderer.setPixelRatio(window.devicePixelRatio)
 	renderer.setSize(window.innerWidth, window.innerHeight)
-	camera.position.setZ(590)
-	camera.position.setY(90)
-	camera.position.setX(90)
+	camera.position.setZ(1)
+	camera.position.setY(1)
+	camera.position.setX(2)
 }
 
-function getModelData(space) {
+function getModelData() {
 	let coordinates = []
 	let colors = []
-	if (space === 'oklab') {
-		let lch = useMode(modeLch)
-		for (let x = 0; x <= 1; x += 0.01) {
-			for (let y = 0; y <= 1; y += 0.01) {
-				for (let z = 0; z <= 1; z += 0.01) {
-					let rgb = { mode: 'rgb', r: x, g: y, b: z }
-					let color = lch(rgb)
-					if (color.l && color.c && color.h) {
-						coordinates.push(new Vector3(color.l, color.c, color.h))
-						colors.push(rgb.r, rgb.g, rgb.b)
-					}
+	let lch = useMode(modeLch)
+	for (let x = 0; x <= 1; x += 0.01) {
+		for (let y = 0; y <= 1; y += 0.01) {
+			for (let z = 0; z <= 1; z += 0.01) {
+				let rgb = { mode: 'rgb', r: x, g: y, b: z }
+				let color = lch(rgb)
+				if (color.l && color.c && color.h) {
+					coordinates.push(new Vector3(rgb.r, rgb.g, rgb.b))
+					colors.push(rgb.r, rgb.g, rgb.b)
 				}
 			}
 		}
 	}
-	else if (space === 'srgb') {
-		let lch = useMode(modeLch)
-		for (let x = 0; x <= 1; x += 0.01) {
-			for (let y = 0; y <= 1; y += 0.01) {
-				for (let z = 0; z <= 1; z += 0.01) {
-					let rgb = { mode: 'rgb', r: x, g: y, b: z }
-					let color = lch(rgb)
-					if (color.l && color.c && color.h) {
-						coordinates.push(new Vector3(rgb.r, rgb.g, rgb.b))
-						colors.push(rgb.r, rgb.g, rgb.b)
-					}
-				}
-			}
-		}
-	}
-	else if (space === 'oklch') {
-		let oklch = useMode(modeOklch)
-		for (let x = 0; x <= 1; x += 0.01) {
-			for (let y = 0; y <= 1; y += 0.01) {
-				for (let z = 0; z <= 1; z += 0.01) {
-					let rgb = { mode: 'rgb', r: x, g: y, b: z }
-					let color = oklch(rgb)
-					if (color.l && color.c && color.h) {
-						coordinates.push(new Cylindrical(color.l, color.c, color.h))
-						colors.push(rgb.r, rgb.g, rgb.b)
-					}
-				}
-			}
-		}
-	}
-
-	return { space: space, coordinates, colors }
+	return { coordinates, colors }
 }
 
-function srgbInOklab() {
-	const modelData = getModelData('oklab')
+function generateModel() {
+	const modelData = getModelData()
 	const geometry = new BufferGeometry().setFromPoints(modelData.coordinates)
-	const color = new Float32Array(modelData.colors)
-	geometry.setAttribute('color', new BufferAttribute(color, 3))
-	const material = new MeshBasicMaterial({ vertexColors: true })
-	const points = new Points(geometry, material)
-	controls = new TrackballControls(camera, renderer.domElement)
-	scene.add(points)
-}
-
-function srgbInSrgb() {
-	const modelData = getModelData('srgb')
-	const geometry = new BufferGeometry().setFromPoints(modelData.coordinates)
-	const color = new Float32Array(modelData.colors)
-	geometry.setAttribute('color', new BufferAttribute(color, 3))
-	const material = new MeshBasicMaterial({ vertexColors: true })
-	const points = new Points(geometry, material)
-	controls = new TrackballControls(camera, renderer.domElement)
-	scene.add(points)
-}
-
-function srgbInOklch() {
-	const modelData = getModelData('oklch')
-	let positions = []
-	for (let i in modelData.coordinates) {
-		let coordinates = modelData.coordinates[i]
-		let x = coordinates.radius * Math.sin(coordinates.y) * Math.cos(coordinates.theta)
-		let y = coordinates.radius * Math.sin(coordinates.y) * Math.sin(coordinates.theta)
-		let z = coordinates.radius * Math.cos(coordinates.y)
-		positions.push(new Vector3(x, y, z))
-	}
-	const geometry = new BufferGeometry().setFromPoints(positions)
-	const color = new Float32Array(modelData.colors)
-	geometry.setAttribute('color', new BufferAttribute(color, 3))
-	const material = new MeshBasicMaterial({ vertexColors: true })
+	const material = new MeshBasicMaterial()
 	const points = new Points(geometry, material)
 	controls = new TrackballControls(camera, renderer.domElement)
 	scene.add(points)
@@ -127,9 +60,7 @@ function animate() {
 
 function drawFigure() {
 	init()
-	//srgbInSrgb()
-	srgbInOklab()
-	//srgbInOklch()
+	generateModel()
 	animate()
 }
 
